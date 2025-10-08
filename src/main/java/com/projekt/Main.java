63b28d7bc03ecc728d0acb4f752e5462c7fbd47f
ddeejjvviidd <david.salek@tul.cz>
2025-10-08 11:32:56 +0200

    #5 Builder type class CustomDrink, tests

--- src/main/java/com/projekt/Main.java
+++ src/main/java/com/projekt/Main.java
@@ -5,7 +5,12 @@ public class Main {
         
         System.out.println("Welcome to " + CafeConfig.getInstance().getCafeName());
 
+        // Factory method, 2.cv
         System.out.println(DrinkFactory.createDrink("coffee").serve() + " in: " + CafeConfig.getInstance().getCafeName());
         System.out.println(DrinkFactory.createDrink("cola").serve());
+
+        // Builder class, 3.cv
+        CustomDrink meinCustomDrink = new CustomDrink.Builder("coffee").milk().sugar().caramel().build();
+        System.out.println(meinCustomDrink + " in " + CafeConfig.getInstance().getCafeName());
     }
 }
